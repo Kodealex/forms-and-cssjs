852d8c4175026e15bcfab6ab884b26ee9d6e1f85
Kodealex <kodeloftet_alexander@hotmail.com>
2024-11-26 12:09:22 +0100

    oppdatering

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const Box = document.querySelector('#Grønnbox');
-const button = document.querySelector('#Kjør');
+// const Box = document.querySelector('#Grønnbox');
+// const button = document.querySelector('#Kjør');
 
-button.addEventListener('click', function () {
-  Box.classList.toggle('Rødbox');
-  Box.classList.toggle('Blåbox');
+// button.addEventListener('click', function () {
+  // Box.classList.toggle('Rødbox');
+  // Box.classList.toggle('Blåbox');
+// });
+
+const Button2 = document.querySelector("#submitButton");
+
+Button2.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log("Button is clicked");
 });
